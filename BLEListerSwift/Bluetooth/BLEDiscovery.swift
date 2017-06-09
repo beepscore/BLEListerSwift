99836ecd2081b6df8ee4592048dfe7ffa7c6fef6
beepscore <support@beepscore.com>
2017-06-09 15:11:51 -0700

    In BLEDiscovery continue implement centralManagerDidUpdateState. In BLEDiscoveryTests add tests.

--- BLEListerSwift/Bluetooth/BLEDiscovery.swift
+++ BLEListerSwift/Bluetooth/BLEDiscovery.swift
@@ -9,6 +9,11 @@
 import Foundation
 import CoreBluetooth
 
+struct BLEDiscoveryConstants {
+    static let didRefreshNotification = "didRefreshNotification"
+    static let statePoweredOffNotification = "statePoweredOffNotification"
+}
+
 class BLEDiscovery: NSObject {
 
     var centralManager: CBCentralManager? = nil
@@ -16,10 +21,16 @@ class BLEDiscovery: NSObject {
     var connectedServices: [CBService]? = []
     var notificationCenter: NotificationCenter? = nil
 
+    var isFirstRun = true
+
     // https://stackoverflow.com/questions/39628277/singleton-with-swift-3-0?noredirect=1&lq=1
     // https://stackoverflow.com/questions/37953317/singleton-with-properties-in-swift-3
     static let shared: BLEDiscovery = {
-        let instance = BLEDiscovery(withCentralManager: CBCentralManager(),
+
+        // queue nil uses main queue
+        let cm = CBCentralManager(delegate: self as? CBCentralManagerDelegate, queue: nil)
+
+        let instance = BLEDiscovery(withCentralManager: cm,
                                     foundPeripherals: [CBPeripheral](),
                                     connectedServices: [CBService](),
                                     notificationCenter: NotificationCenter.default)
@@ -40,13 +51,91 @@ class BLEDiscovery: NSObject {
         self.connectedServices = connectedServices
         self.notificationCenter = notificationCenter
     }
-
 }
 
 extension BLEDiscovery: CBCentralManagerDelegate {
 
-    func centralManagerDidUpdateState(_: CBCentralManager) {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
 
+        print(String(describing:central) + "central.state: " + String(describing:central.state))
+
+        switch central.state {
+
+        case .unknown:
+            // wait for another event
+            break
+
+        case .resetting:
+            clearDevices()
+            self.postDidRefresh(notificationCenter: self.notificationCenter)
+            //[peripheralDelegate alarmServiceDidReset];
+
+        case .unsupported:
+            // original code didn't list this case and Xcode warned
+            // so list case to silence warning, but don't do anything
+            break
+
+        case .unauthorized:
+            // Tell user the app is not allowed
+            break
+
+        case .poweredOff:
+            self.clearDevices()
+            self.postDidRefresh(notificationCenter: self.notificationCenter)
+
+            // Tell user to power ON BT for functionality, but not on first run
+            // the Framework will alert in that instance.
+            if !isFirstRun {
+                self.postPoweredOff(notificationCenter: self.notificationCenter)
+            }
+
+        case .poweredOn:
+            self.loadSavedDevices()
+
+            // FIXME: specify services argument
+            //NSArray *peripherals = [central retrieveConnectedPeripheralsWithServices:@[]];
+
+            // Add to list.
+            //            for (CBPeripheral *peripheral in peripherals) {
+            //                // method documentation: Attempts to connect to a peripheral do not time out.
+            //                [central connectPeripheral:peripheral options:nil];
+            //            }
+            self.postDidRefresh(notificationCenter: self.notificationCenter)
+        }
+
+        isFirstRun = false
     }
 
+    /// Uses dependency injection
+    /// - Parameter notificationCenter
+    func postDidRefresh(notificationCenter: NotificationCenter?) {
+        guard let nc = notificationCenter else {
+            return
+        }
+        nc.post(name: NSNotification.Name(rawValue: BLEDiscoveryConstants.didRefreshNotification),
+                object: self,
+                userInfo: nil)
+    }
+
+    /// Uses dependency injection
+    /// - Parameter notificationCenter
+    func postPoweredOff(notificationCenter: NotificationCenter?) {
+        guard let nc = notificationCenter else {
+            return
+        }
+        nc.post(name: NSNotification.Name(rawValue: BLEDiscoveryConstants.didRefreshNotification),
+                object: self,
+                userInfo: nil)
+    }
+    
+    
+    func clearDevices() {
+        // FIXME: implement
+    }
+    
+    func loadSavedDevices() {
+        // FIXME: implement
+        //
+    }
+    
 }
--- BLEListerSwiftTests/BLEDiscoveryTests.swift
+++ BLEListerSwiftTests/BLEDiscoveryTests.swift
@@ -10,7 +10,7 @@ import XCTest
 @testable import BLEListerSwift
 
 class BLEDiscoveryTests: XCTestCase {
-    
+
     func testBLEDiscoverySharedNotNil () {
         let shared0 = BLEDiscovery.shared
         XCTAssertNotNil(shared0)
@@ -40,6 +40,50 @@ class BLEDiscoveryTests: XCTestCase {
     func testSharedNotificationCenter () {
         let shared = BLEDiscovery.shared
         XCTAssertEqual(shared.notificationCenter, NotificationCenter.default)
+    }
+
+    // MARK: test post notifications
+
+    func testPostDidRefresh () {
+        // https://developer.apple.com/documentation/xctest/asynchronous_tests_and_expectations/testing_asynchronous_operations_with_expectations
+        XCTAssertTrue(waitForDidRefreshNotification() == .completed)
+    }
+
+    func waitForDidRefreshNotification () -> XCTWaiterResult {
+        let shared = BLEDiscovery.shared
+        let ncDefault = NotificationCenter.default
+
+        // expectation is fulfilled when notification is posted
+        let expectation = XCTNSNotificationExpectation(name: BLEDiscoveryConstants.didRefreshNotification,
+                                                       object: shared,
+                                                       notificationCenter: ncDefault)
+
+        // call method under test
+        shared.postDidRefresh(notificationCenter: shared.notificationCenter)
+
+        let result = XCTWaiter().wait(for: [expectation], timeout: 1)
+        return result
+    }
+
+    /// Asynchronous test
+    /// https://developer.apple.com/documentation/xctest/asynchronous_tests_and_expectations/testing_asynchronous_operations_with_expectations
+    /// Alternatively could use more verbose XCTWaiter and check it returns .completed
+    /// http://masilotti.com/xctest-waiting/
+    /// http://shashikantjagtap.net/asynchronous-ios-testing-swift-xcwaiter/
+    func testPostDidRefresh2 () {
+        let shared = BLEDiscovery.shared
+        let ncDefault = NotificationCenter.default
+
+        // XCTNSNotificationExpectation is fulfilled iff notification is posted
+        let expectation = XCTNSNotificationExpectation(name: BLEDiscoveryConstants.didRefreshNotification,
+                                                       object: shared,
+                                                       notificationCenter: ncDefault)
+
+        // call method under test
+        shared.postDidRefresh(notificationCenter: shared.notificationCenter)
+
+        // wait until the expectation if fulfilled
+        wait(for: [expectation], timeout: 1.0)
     }
 
 }
