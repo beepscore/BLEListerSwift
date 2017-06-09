921d6f8e3269a7728ce069ac9dedaeafd084f7fd
beepscore <support@beepscore.com>
2017-06-08 20:07:55 -0700

    Add BLEDiscovery. TODO: Implement this similar to BLELister BSBLeDiscovery

---
+++ BLEListerSwift/Bluetooth/BLEDiscovery.swift
@@ -0,0 +1,13 @@
+//
+//  BLEDiscovery.swift
+//  BLEListerSwift
+//
+//  Created by Steve Baker on 6/8/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+import Foundation
+
+class BLEDiscovery: NSObject {
+
+}
